94176f16a431c53599c312809cf0bd46c6a3dee7
BabySeeME「大雄」 <417170808@qq.com>
2021-06-24 15:44:31 +0800

    first commit

---
+++ publish/sms.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+
+return [
+    "username" => env('SMS_USERNAME', 'username'),
+    "password" => env('SMS_PASSWORD', 'password'),
+    "epid"     => env('SMS_EPID', 'epid'),
+];
